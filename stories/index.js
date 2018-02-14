cd54be40a8bb90dc7b5c688bd4df89aed2b87b7b
kennethormandy <hello@kennethormandy.com>
2018-02-13 23:13:11 -0800

    Updates Stories with real working exmaples

--- stories/index.js
+++ stories/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { setDefaults, withInfo } from '@storybook/addon-info'
+import { State, Store } from '@sambego/storybook-state'
 
 import Flipcard from '../src/Flipcard.js'
 import '../src/Flipcard.css'
@@ -9,81 +9,92 @@ import './stories.css'
 
 let padding = '5vw'
 
-setDefaults({
-  header: true, // Toggles display of header with component name and description
-  inline: true, // Displays info inline vs click button to view
-  source: true, // Displays the source of story Component
-  propTables: false,
-  // https://github.com/storybooks/storybook/blob/master/addons/info/src/components/Story.js#L19
-  styles: {
-    infoBody: {
-      fontWeight: 400,
-      boxShadow: 'none',
-      // marginTop: '100px',
-      padding: `1rem ${padding}`,
-      borderRadius: 0,
-      borderWidth: '0',
-    },
-    infoStory: {
-      background: '#000',
-      color: '#FFF',
-      padding: padding,
-      overflow: 'hidden',
-    },
-    jsxInfoContent: { margin: 0 },
-    header: {
-      h1: {
-        fontSize: '1rem',
-        lineHeight: 1.5,
-        display: 'inline-block',
-        margin: 0,
-        paddingBottom: '5px',
-        paddingRight: '0.25em',
-        fontWeight: 700,
-      },
-      h2: {
-        fontSize: '1rem',
-        lineHeight: 1.5,
-        display: 'inline-block',
-        margin: 0,
-        paddingBottom: '5px',
-        fontWeight: 400,
-      },
-      body: {
-        margin: 0,
-        paddingTop: 0,
-      },
-    },
-    source: {
-      h1: { fontSize: '1rem', fontWeight: 700, lineHeight: 1.5 },
-    },
-  },
+const store = new Store({
+  flipped: false,
 })
 
+const Button = props => (
+  <button
+    {...props}
+    style={{
+      borderRadius: '999px',
+      backgroundColor: 'cornflowerblue',
+      color: 'white',
+      padding: '1em 2.5em',
+      fontSize: '1rem',
+      marginBottom: '1em',
+      borderColor: 'transparent',
+      cursor: 'pointer',
+    }}>
+    {props.children}
+  </button>
+)
+
+Button.defaultProps = {
+  children: 'Flip',
+  onClick: () => store.set({ flipped: !store.get('flipped') }),
+}
+
+const Card = props => {
+  return (
+    <div
+      style={{
+        fontSize: '10rem',
+        fontWeight: 600,
+        background: 'white',
+        color: 'cornflowerblue',
+        padding: '0',
+        position: 'relative',
+      }}>
+      <div style={{ padding: '3rem' }}>{props.children}</div>
+    </div>
+  )
+}
+
 storiesOf('Flipcard', module)
-  .add(
-    'with defaults',
-    withInfo('')(() => (
-      <Flipcard>
-        <div>
-          <h1>One</h1>
-        </div>
-        <div>
-          <h1>Two</h1>
-        </div>
-      </Flipcard>
-    ))
-  )
-  .add(
-    'with revolving door transition',
-    withInfo('')(() => (
-      <Flipcard type="revolving-door">
-        <div>
-          <h1>One</h1>
-        </div>
-        <div>
-          <h1>Two</h1>
-        </div>
-      </Flipcard>
-    ))
-  )
+  .add('with defaults', () => (
+    <div>
+      <Button />
+      <State store={store}>
+        <Flipcard>
+          <Card>One</Card>
+          <Card>Two</Card>
+        </Flipcard>
+      </State>
+    </div>
+  ))
+  .add('with revolving door transition', () => {
+    // Reset Store on render for this demo
+    store.set({ flipped: false })
+
+    return (
+      <div>
+        <Button />
+        <State store={store}>
+          <Flipcard type="revolving-door">
+            <Card>One</Card>
+            <Card>Two</Card>
+          </Flipcard>
+        </State>
+      </div>
+    )
+  })
+  .add('with no styling', () => (
+    <div>
+      <Button />
+      <State store={store}>
+        <Flipcard>
+          <h1>h1 One</h1>
+          <h1>h1 Two</h1>
+        </Flipcard>
+      </State>
+      <p style={{ maxWidth: '500px', marginTop: '7rem' }}>
+        This component includes as little styling opinions as necessary—nearly
+        all the styles for these examples is included within the tests, rather
+        than within the React component. Note the button isn’t part of the
+        component either—you can use any kind of button or input you want to
+        trigger the flip by passing the <code>flipped</code> prop to{' '}
+        <code>&lt;Flipcard/&gt;</code>.
+      </p>
+    </div>
+  ))
